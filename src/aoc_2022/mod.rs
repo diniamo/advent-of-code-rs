88e05d4b54327f5ceceab61a8151ef736e24d47f
diniamo <diniamo69@gmail.com>
2022-12-03 10:07:56 +0100

    day 2

--- src/aoc_2022/mod.rs
+++ src/aoc_2022/mod.rs
@@ -1,6 +1,7 @@
 use advent_of_code::*;
 
-pub mod day_1;
+mod day_1;
+mod day_2;
 
 pub struct Aoc2022 {}
 impl Year for Aoc2022 {
@@ -10,7 +11,8 @@ impl Year for Aoc2022 {
 
     fn days(&self) -> Vec<Box<dyn Day>> {
         vec![
-            Box::new(day_1::Day1 {})
+            Box::new(day_1::Day1 {}),
+            Box::new(day_2::Day2 {})
         ]
     }
 }
