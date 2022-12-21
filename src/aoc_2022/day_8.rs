b9c4f096555c7c2abe735014ca010fcb56937607
diniamo <diniamo69@gmail.com>
2022-12-21 16:28:13 +0100

    day 8 cleanup

--- src/aoc_2022/day_8.rs
+++ src/aoc_2022/day_8.rs
@@ -11,24 +11,14 @@ impl Day for Day8 {
     }
 
     fn part1(&self, input: &String) -> String {
-//         let input = "30373
-// 25512
-// 65332
-// 33549
-// 35390";
         let map: Vec<Vec<u32>> = input.lines().map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect()).collect();
 
         // map.iter().map(|row| row.iter().map(|digit| digit.to_string()).collect()).map(|row: Vec<String>| row.join(" ")).for_each(|line| println!("{line}"));
-        // println!();
-
 
         // The initial value here is the perimeter of the grid, as we don't account for that in the loop
         let mut visible: usize = 2*(map.len() - 2 + map[0].len() - 2) + 4;
-        // println!("{visible}");
         for (i, row) in (&map[1..(map.len() - 1)]).iter().enumerate() {
             for (j, &digit) in (&row[1..(row.len() - 1)]).iter().enumerate() {
-                // print!("{digit} at ({j}, {i}) in row {:?}", row);
-                // print!(" with if values {}, {}, {}, {}", (&row[..=j]).iter().all(|&d| d < digit), (&row[(j + 2)..]).iter().all(|&d| d < digit), (&map[..=i]).iter().all(|r| r[j + 1] < digit), (&map[(i + 2)..]).iter().all(|r| r[j + 1] < digit));
 
                 // Left, right, up, down
                 if (&row[..=j]).iter().all(|&d| d < digit)    // Wouldn't be inclusive, but we start from row[1] and j is still 0
@@ -36,8 +26,7 @@ impl Day for Day8 {
                     || (&map[..=i]).iter().all(|r| r[j + 1] < digit)    // Wouldn't be inclusive, but we start from map[1] and i is still 0
                     || (&map[(i + 2)..]).iter().all(|r| r[j + 1] < digit) {    // Would be i+1 and j, reason(s) above
                     visible += 1;
-                    // println!(" is visible!")
-                }/*  else { println!(); }  */
+                }
                 
             }
         }
@@ -46,21 +35,13 @@ impl Day for Day8 {
     }
 
     fn part2(&self, input: &String) -> String {
-//         let input = "30373
-// 25512
-// 65332
-// 33549
-// 35390";
         let map: Vec<Vec<u32>> = input.lines().map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect()).collect();
 
-        map.iter().map(|row| row.iter().map(|digit| digit.to_string()).collect()).map(|row: Vec<String>| row.join(" ")).for_each(|line| println!("{line}"));
-        println!();
+        // map.iter().map(|row| row.iter().map(|digit| digit.to_string()).collect()).map(|row: Vec<String>| row.join(" ")).for_each(|line| println!("{line}"));
 
         let mut highest: usize = 0;
         for (i, row) in (&map[1..(map.len() - 1)]).iter().enumerate() {
             for (j, &digit) in (&row[1..(row.len() - 1)]).iter().enumerate() {
-                println!("{digit} at ({j}, {i}) in row {:?}", row);
-
                 let left_side = &row[..=j];
                 let left = left_side.iter().rev().enumerate().find_map(|(k, &d)| if digit <= d || k == (left_side.len() - 1) { Some(k + 1) } else { None }).unwrap();
                 let right_side = &row[(j + 2)..];
